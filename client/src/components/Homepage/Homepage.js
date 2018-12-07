48c9c59da6a68e04cf03acad10115462213bb305
majorazero <daniel.cw.hsu@gmail.com>
2018-12-07 14:53:11 -0800

    added basic form functionaltiy to front page

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -5,19 +5,49 @@ class Homepage extends Component {
     super();
 
     this.state = {
-
+      destination: "",
+      startDate: "",
+      endDate: ""
     };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
   }
 
   render(){
     return(
       <div>
-        <h1>HOMEPAGE</h1>
         <div>
           <a href="/login">Login</a>
         </div>
         <div>
           <a href="/register">Register</a>
+        </div>
+        <div className="jumbotron">
+          <h1>Where do you want to go?</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label>Destination</label>
+              <input onChange={this.handleChange} className="form-control" name="destination" type="text" />
+            </div>
+            <div className="form-group">
+              <label>Start Date</label>
+              <input onChange={this.handleChange} className="form-control" name="startDate" type="date" />
+            </div>
+            <div className="form-group">
+              <label>End Date</label>
+              <input onChange={this.handleChange} className="form-control" name="endDate" type="date" />
+            </div>
+            <button type="submit">Lets Go!</button>
+          </form>
         </div>
       </div>
     );
