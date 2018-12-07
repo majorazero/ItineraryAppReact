2d883b7d8f0a4545b658019d15294f175469af53
majorazero <daniel.cw.hsu@gmail.com>
2018-12-06 17:31:33 -0800

    added a homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Homepage from "./components/Homepage/Homepage.js";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage}/>
+        </Switch>
+      </Router>
     );
   }
 }
